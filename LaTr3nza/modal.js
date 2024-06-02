dbd252060c1c2e1276429d440efe82fd461a2920
MacaVitaSanchez <macavitasanchez@gmail.com>
2024-06-02 08:12:42 -0300

    arreglo errores

--- LaTr3nza/modal.js
+++ LaTr3nza/modal.js
@@ -1,24 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Obtén todas las imágenes del carrusel
     var images = document.querySelectorAll('.carousel-item img');
 
-    // Obtén el modal
     var modal = document.getElementById('imageModal');
 
-    // Obtén el elemento de la imagen dentro del modal
     var modalImg = document.getElementById("modalImage");
 
-    // Recorre todas las imágenes del carrusel y añade un evento de clic a cada una
     images.forEach(function(image) {
         image.addEventListener('click', function() {
-            modal.style.display = "block"; // Muestra el modal
-            modalImg.src = this.src; // Establece la imagen en el modal
+            modal.style.display = "block"; 
+            modalImg.src = this.src; 
         });
     });
 
-    // Obtiene el elemento de cierre (x) del modal y agrega un evento de clic para cerrar el modal
     var closeBtn = document.querySelector(".close");
     closeBtn.addEventListener('click', function() {
-        modal.style.display = "none"; // Oculta el modal cuando se hace clic en el botón de cerrar
+        modal.style.display = "none"; 
     });
 });
